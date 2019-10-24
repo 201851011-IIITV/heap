acc62e4f274425567bd8905ffb4b47df5cf9e2e6
201851019-iiitv <201851019@iiitvadodara.ac.in>
2019-10-24 18:22:29 +0530

    update code And Readme.md file

--- Heap.java
+++ Heap.java
@@ -1,10 +1,10 @@
 
 
 public interface Heap {
-	public void Insert(int k);
-	public int Deletion();
-	public void getMax();
-	public void traverseHeap();
+	public void Insert(int k); //new number insert heap max tree;
+	public int Deletion(); //delete last node;
+	public void getMax(); // print parent node;
+	public void traverseHeap(); //for print the max heap;
 
 
 }
